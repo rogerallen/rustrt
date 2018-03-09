e388206719e780102c5b37a21a3bec900c9f1b21
Roger Allen <rallen@gmail.com>
2018-03-09 07:56:06 -0800

    Finish Chapter 3.  Got a blue/white sky.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 // cargo run > out.ppm
 // Chapter 1 - done
 mod vec3;
-use vec3::Vec3;
+use vec3::{Vec3,unit_vector};
+mod ray;
+use ray::Ray;
+
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction = unit_vector(r.direction());
+    let t = 0.5*(unit_direction.y() + 1.0);
+    (1.0-t)*Vec3::new(1.0, 1.0, 1.0) + t*Vec3::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     let nx = 200;
     let ny = 100;
     println!("P3\n{0} {1} 255",nx,ny);
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new( (i as f32)/(nx as f32), (j as f32)/(ny as f32), 0.2 as f32 );
+            let u = (i as f64)/(nx as f64);
+            let v = (j as f64)/(ny as f64);
+            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+            let col = color(&r);
             let ir = (255.99*col[0]) as i32;
             let ig = (255.99*col[1]) as i32;
             let ib = (255.99*col[2]) as i32;
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,24 +1,72 @@
-use std::ops::Index;
+use std::ops::{Add,Div,Index,Mul,Sub};
 
+#[derive(Clone, Copy, Debug)]
 pub struct Vec3 {
-    e: [f32; 3]
+    e: [f64; 3]
 }
 
 impl Vec3 {
-    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
         Vec3 { e: [x, y, z] }
     }
-    /* unused for the moment
-    pub fn x(&self) -> f32 { self.e[0] }
-    pub fn y(&self) -> f32 { self.e[1] }
-    pub fn z(&self) -> f32 { self.e[2] }
-    pub fn r(&self) -> f32 { self.e[0] }
-    pub fn g(&self) -> f32 { self.e[1] }
-    pub fn b(&self) -> f32 { self.e[2] }
-     */
+    pub fn x(&self) -> f64 { self.e[0] }
+    pub fn y(&self) -> f64 { self.e[1] }
+    pub fn z(&self) -> f64 { self.e[2] }
+    pub fn r(&self) -> f64 { self.e[0] }
+    pub fn g(&self) -> f64 { self.e[1] }
+    pub fn b(&self) -> f64 { self.e[2] }
+    pub fn length(&self) -> f64 {
+        (self.e[0] * self.e[0] + self.e[1]*self.e[1] + self.e[2]*self.e[2]).sqrt()
+    }
 }
 
 impl Index<usize> for Vec3 {
-    type Output = f32;
-    fn index(&self, i: usize) -> &f32 { &self.e[i] }
+    type Output = f64;
+    fn index(&self, i: usize) -> &f64 { &self.e[i] }
+}
+
+impl Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, v2: Vec3) -> Vec3 {
+        Vec3::new(self.e[0] + v2.e[0], self.e[1] + v2.e[1], self.e[2] + v2.e[2])
+    }
+}
+
+impl Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, v2: Vec3) -> Vec3 {
+        Vec3::new(self.e[0] - v2.e[0], self.e[1] - v2.e[1], self.e[2] - v2.e[2])
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, v2: Vec3) -> Vec3 {
+        Vec3::new(self.e[0] * v2.e[0], self.e[1] * v2.e[1], self.e[2] * v2.e[2])
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self: f64, v2: Vec3) -> Vec3 {
+        Vec3::new(self * v2.e[0], self * v2.e[1], self * v2.e[2])
+    }
+}
+
+impl Div<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn div(self, v2: Vec3) -> Vec3 {
+        Vec3::new(self.e[0] / v2.e[0], self.e[1] / v2.e[1], self.e[2] / v2.e[2])
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, t: f64) -> Vec3 {
+        Vec3::new(self.e[0] / t, self.e[1] / t, self.e[2] / t)
+    }
+}
+
+pub fn unit_vector(v: Vec3) -> Vec3 {
+    v / v.length()
 }
