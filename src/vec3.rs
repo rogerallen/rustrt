289ba97af5ae6e17e5fee7ec121acdac34cc75f0
Roger Allen <rallen@gmail.com>
2018-03-08 21:34:11 -0800

    Just enough Vec3 to compile Chapter 2.

---
+++ src/vec3.rs
@@ -0,0 +1,24 @@
+use std::ops::Index;
+
+pub struct Vec3 {
+    pub e: [f32; 3]
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { e: [x, y, z] }
+    }
+    /* unused for the moment
+    pub fn x(&self) -> f32 { self.e[0] }
+    pub fn y(&self) -> f32 { self.e[1] }
+    pub fn z(&self) -> f32 { self.e[2] }
+    pub fn r(&self) -> f32 { self.e[0] }
+    pub fn g(&self) -> f32 { self.e[1] }
+    pub fn b(&self) -> f32 { self.e[2] }
+     */
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+    fn index(&self, i: usize) -> &f32 { &self.e[i] }
+}
