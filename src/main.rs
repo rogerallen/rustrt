8b7c023ca26fb14ff80f7aa2bc8f814f1e726b71
Roger Allen <rallen@gmail.com>
2018-03-13 13:39:53 -0700

    Chapter 7 done.

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,23 @@ use vec3::{unit_vector, Vec3};
 use std::f64;
 use rand::Rng;
 
+fn random_in_unit_sphere() -> Vec3 {
+    let mut rng = rand::thread_rng();
+    // no do..while in rust.  :(
+    let mut p = 2.0 * Vec3::new(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>())
+        - Vec3::new(1.0, 1.0, 1.0);
+    while p.squared_length() >= 1.0 {
+        p = 2.0 * Vec3::new(rng.gen::<f64>(), rng.gen::<f64>(), rng.gen::<f64>())
+            - Vec3::new(1.0, 1.0, 1.0);
+    }
+    p
+}
+
 fn color(r: &Ray, world: &HitableList) -> Vec3 {
     let mut rec = HitRecord::new();
-    if world.hit(r, 0.0, f64::MAX, &mut rec) {
-        0.5
-            * Vec3::new(
-                rec.normal.x() + 1.0,
-                rec.normal.y() + 1.0,
-                rec.normal.z() + 1.0,
-            )
+    if world.hit(r, 0.001, f64::MAX, &mut rec) {
+        let target = rec.p + rec.normal + random_in_unit_sphere();
+        0.5 * color(&Ray::new(rec.p, target - rec.p), world)
     } else {
         let unit_direction = unit_vector(r.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -49,11 +57,12 @@ fn main() {
             for _s in 0..ns {
                 let u = (i as f64 + rng.gen::<f64>()) / (nx as f64);
                 let v = (j as f64 + rng.gen::<f64>()) / (ny as f64);
-                let r = cam.get_ray(u,v);
+                let r = cam.get_ray(u, v);
                 // ??? let p = r.point_at_parameter(2.0);
                 col += color(&r, &world);
             }
             col /= ns as f64;
+            col = Vec3::new(col[0].sqrt(), col[1].sqrt(), col[2].sqrt());
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
             let ib = (255.99 * col[2]) as i32;
