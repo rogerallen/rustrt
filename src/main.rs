518d5fc2b25601937a44f0d8ea45048f3625b4ab
Roger Allen <rallen@gmail.com>
2018-03-13 11:46:37 -0700

    Chapter 6 - Antialiasing added.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 // cargo run > out.ppm
+extern crate rand;
+
+mod camera;
 mod hitable;
 mod hitable_list;
 mod ray;
 mod sphere;
 mod vec3;
 
+use camera::Camera;
 use hitable::{HitRecord, Hitable};
 use hitable_list::HitableList;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::{unit_vector, Vec3};
 use std::f64;
+use rand::Rng;
 
 fn color(r: &Ray, world: &HitableList) -> Vec3 {
     let mut rec = HitRecord::new();
@@ -29,23 +34,26 @@ fn color(r: &Ray, world: &HitableList) -> Vec3 {
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
     let nx = 200;
     let ny = 100;
+    let ns = 100;
     println!("P3\n{0} {1} 255", nx, ny);
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
     let mut world = HitableList::new();
     world.push(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5));
     world.push(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0));
+    let cam = Camera::new();
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u = (i as f64) / (nx as f64);
-            let v = (j as f64) / (ny as f64);
-            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            // ??? let p = r.point_at_parameter(2.0);
-            let col = color(&r, &world);
+            let mut col = Vec3::new(0.0, 0.0, 0.0);
+            for _s in 0..ns {
+                let u = (i as f64 + rng.gen::<f64>()) / (nx as f64);
+                let v = (j as f64 + rng.gen::<f64>()) / (ny as f64);
+                let r = cam.get_ray(u,v);
+                // ??? let p = r.point_at_parameter(2.0);
+                col += color(&r, &world);
+            }
+            col /= ns as f64;
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
             let ib = (255.99 * col[2]) as i32;
