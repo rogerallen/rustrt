87d0bb20587cbf40b09f73b22d840f2a36d6161d
Roger Allen <rallen@gmail.com>
2018-03-15 20:17:09 -0700

    close to final scene

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,7 @@ fn color(r: &Ray, world: &HitableList, depth: i32) -> Vec3 {
     }
 }
 
-fn main() {
-    let mut rng = rand::thread_rng();
-    let nx = 200;
-    let ny = 100;
-    let ns = 100;
-    println!("P3\n{0} {1} 255", nx, ny);
-    let mut world = HitableList::new();
+fn original_scene(world: &mut HitableList) {
     /* original world */
     world.push(Sphere::new(
         Vec3::new(0.0, 0.0, -1.0),
@@ -80,36 +74,84 @@ fn main() {
         -0.45, // makes a bubble inside sphere
         Material::Dielectric { ref_idx: 1.5 },
     ));
-    /* red/blue ball world
-    let R = (std::f64::consts::PI/4.0).cos();
+}
+
+fn redblue_scene(world: &mut HitableList) {
+    let r = (std::f64::consts::PI/4.0).cos();
     world.push(Sphere::new(
-        Vec3::new(-R, 0.0, -1.0),
-        R,
+        Vec3::new(-r, 0.0, -1.0),
+        r,
         Material::Lambertian {
             albedo: Vec3::new(0.0, 0.0, 1.0),
         },
     ));
     world.push(Sphere::new(
-        Vec3::new(R, 0.0, -1.0),
-        R,
+        Vec3::new(r, 0.0, -1.0),
+        r,
         Material::Lambertian {
             albedo: Vec3::new(1.0, 0.0, 0.0),
         },
     ));
-     */
-    let lookfrom = Vec3::new(3., 3., 2.);
-    let lookat = Vec3::new(0., 0., -1.);
+}
+
+fn final_scene(world: &mut HitableList) {
+    let mut rng = rand::thread_rng();
+    let n = 500;
+    world.push(Sphere::new(Vec3::new(0.,-1000.,0.), 1000., Material::Lambertian { albedo: Vec3::new(0.5, 0.5, 0.5) } ));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rng.gen::<f64>();
+            let center = Vec3::new(a as f64 + 0.9*rng.gen::<f64>(), 0.2, b as f64 + 0.9*rng.gen::<f64>());
+            if (center-Vec3::new(4.,0.2,0.)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    world.push(Sphere::new(center, 0.2,
+                               Material::Lambertian { albedo: Vec3::new(rng.gen::<f64>()*rng.gen::<f64>(), rng.gen::<f64>()*rng.gen::<f64>(), rng.gen::<f64>()*rng.gen::<f64>()) } ));
+                }
+                else if choose_mat < 0.95 {
+                    world.push(Sphere::new(center, 0.2,
+                               Material::Metal { albedo: Vec3::new(0.5*(1. + rng.gen::<f64>()), 0.5*(1. + rng.gen::<f64>()), 0.5*(1. + rng.gen::<f64>())), fuzz: 0.5*rng.gen::<f64>() } ));
+                }
+                else {
+                    world.push(Sphere::new(center, 0.2,
+                                           Material::Dielectric { ref_idx: 1.5 } ));
+                }
+            }
+        }
+    }
+
+    world.push(Sphere::new(Vec3::new(0.,1.,0.), 1., Material::Dielectric { ref_idx: 1.5 } ));
+    world.push(Sphere::new(Vec3::new(-4.,1.,0.), 1., Material::Lambertian { albedo: Vec3::new(0.4, 0.2, 0.1) } ));
+    world.push(Sphere::new(Vec3::new(4.,1.,0.), 1., Material::Metal { albedo: Vec3::new(0.7, 0.6, 0.5), fuzz: 0.0 } ));
+
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let nx = 800;
+    let ny = 400;
+    let ns = 100;
+    println!("P3\n{0} {1} 255", nx, ny);
+
+    let mut world = HitableList::new();
+    //original_scene(&mut world);
+    //redblue_scene(&mut world);
+    final_scene(&mut world);
+
+    let lookfrom = Vec3::new(8., 1.5, 2.);
+    let lookat = Vec3::new(0., 1., -1.);
     let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let aperture = 0.05;
     let cam = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0., 1., 0.),
-        20.0,
+        30.0,
         nx as f64 / ny as f64,
         aperture,
         dist_to_focus,
     );
+
     for j in (0..ny).rev() {
         for i in 0..nx {
             let mut col = Vec3::new(0.0, 0.0, 0.0);
